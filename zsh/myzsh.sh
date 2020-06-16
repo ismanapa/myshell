05cea54d9f81e7ce74bb0c09a5887eb8ce094e80
Ismael Navarro <inavarro@plainconcepts.com>
2020-06-16 18:44:23 +0200

    Added command to clean teams cache

--- zsh/myzsh.sh
+++ zsh/myzsh.sh
@@ -10,4 +10,17 @@ function test_performance () {
   for i in $(seq 1 10); do time zsh -i -c exit; done
 }
 
+function cleanTeamsCache () {
+  baseDir=$APPDATA/Microsoft/Teams
+ 
+  rm -rf $baseDir/Application\ Cache/Cache/*
+  rm -rf $baseDir/Blob_storage/*
+  rm -rf $baseDir/Cache/*
+  rm -rf $baseDir/databases/*
+  rm -rf $baseDir/GPUCache/*
+  rm -rf $baseDir/IndexedDB/*
+  rm -rf $baseDir/Local\ Storage/*
+  rm -rf $baseDir/tmp/*
+}
+
 source $HOME/theme/my-theme.zsh
