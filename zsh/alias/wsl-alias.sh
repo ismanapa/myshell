33243a84ce85bfc26bd640e8c82573db0fe6b498
Ismael Navarro <inavarro@plainconcepts.com>
2020-06-17 10:14:56 +0200

    Added npm alias to wsl

--- zsh/alias/wsl-alias.sh
+++ zsh/alias/wsl-alias.sh
@@ -1,4 +1,6 @@
 alias kubectl=kubectl.exe
 alias dotnet=dotnet.exe
 alias git=git.exe
+alias node=node.exe
+alias npm='cmd.exe /c npm.cmd'
 alias az='cmd.exe /c C:/Program\ Files\ \(x86\)/Microsoft\ SDKs/Azure/CLI2/wbin/az.cmd'
