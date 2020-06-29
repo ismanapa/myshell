a512bc848ed46175c0a99c1d8072931f4f7946bd
Ismael Navarro <inavarro@plainconcepts.com>
2020-06-29 19:15:41 +0200

    Added alias for kubectl port-forward

--- zsh/alias/kube-alias.sh
+++ zsh/alias/kube-alias.sh
@@ -21,3 +21,11 @@ function kubelogs () {
   
   kubectl logs "$pod" -n "$ns" --tail=50 --follow
 } 
+
+function kubeforwardsvc () {
+  ns=$(kubectl get ns | fzf --layout reverse  | awk '{print $1}')
+  svc=$(kubectl get svc -n $ns | fzf --layout reverse  | awk '{print $1}')
+  port=$(kubectl get svc $svc -n $ns --template='{{range .spec.ports}}{{.port}} {{.protocol}}{{"\n"}}{{end}}' | fzf --layout reverse  | awk '{print $1}')
+
+  kubectl port-forward service/"$svc" "$1":"$port" -n "$ns"
+} 
