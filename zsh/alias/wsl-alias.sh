4d577e489f4140d45b216223565fa7a21fb8ccb5
Ismael Navarro Páez <ismanapa@gmail.com>
2021-02-24 16:07:19 +0100

    Added npx alias

--- zsh/alias/wsl-alias.sh
+++ zsh/alias/wsl-alias.sh
@@ -3,4 +3,5 @@ alias dotnet=dotnet.exe
 alias git=git.exe
 alias node=node.exe
 alias npm='cmd.exe /c npm.cmd'
+alias npx='cmd.exe /c npx.cmd'
 alias az='cmd.exe /c C:/Program\ Files\ \(x86\)/Microsoft\ SDKs/Azure/CLI2/wbin/az.cmd'
